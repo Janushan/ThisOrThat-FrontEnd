49a0a7a9a529c2ca6bd701b9c20b374961e167a0
Janu Shan <janu47@live.com>
2019-11-25 20:02:13 +0000

    did npm audit fix and changed file type to react (for prettier)

--- src/Components/Pages/QuestionWrapper/index.js
+++ src/Components/Pages/QuestionWrapper/index.js
@@ -1,19 +1,21 @@
-import React  from 'react';
-import Header from '../../Header';
-import Question from '../../Question';
-import Creator from '../../Creator';
-import Join from '../../Join';
-import ToTSubmit from '../../ToTSubmit';
-import './QuestionWrapper.css';
+import React from "react";
+import Header from "../../Header";
+import Question from "../../Question";
+import Creator from "../../Creator";
+import Join from "../../Join";
+import ToTSubmit from "../../ToTSubmit";
+import "./QuestionWrapper.css";
 
 export default function QuestionWrapper() {
-
-    return(
-        <div>
-            <Header/>
-            <div className="screen">
-                <div className="partition1"><Creator/></div>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <div className="screen">
+        <div className="partition1">
+          {" "}
+          <Question />{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
 }
