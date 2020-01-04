9e1797859a5108d38f8075f3fc4909c47b626c50
Janu Shan <janu47@live.com>
2020-01-04 11:47:43 +0000

    added build folder

---
+++ build/precache-manifest.a65e6a9f54cd907d051ee3b5ecdf71f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8b0fa67967b43d7424855f53fb3720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5ee7e58d29f7214feb3",
+    "url": "/static/css/main.e1f775fd.chunk.css"
+  },
+  {
+    "revision": "0cbb47975d6b6d2f6dee",
+    "url": "/static/js/2.75f909aa.chunk.js"
+  },
+  {
+    "revision": "5eb688ccf1e610a788cc7175677efd8c",
+    "url": "/static/js/2.75f909aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5ee7e58d29f7214feb3",
+    "url": "/static/js/main.602fdec5.chunk.js"
+  },
+  {
+    "revision": "2ddbfc8f025ebc487639",
+    "url": "/static/js/runtime-main.79b955ce.js"
+  },
+  {
+    "revision": "611601d3a2a918ba5ffa595feb15961c",
+    "url": "/static/media/ice-cream.611601d3.jpg"
+  },
+  {
+    "revision": "002d59517f71f0965a4d49789463a519",
+    "url": "/static/media/pizza.002d5951.jpg"
+  }
+]);
