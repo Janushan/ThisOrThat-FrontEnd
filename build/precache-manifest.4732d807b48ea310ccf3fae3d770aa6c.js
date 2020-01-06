ea7907b739f5eed6bbe957593c2db4a00e1d7e02
Lunatic1083 <57877628+Lunatic1083@users.noreply.github.com>
2020-01-06 11:27:13 -0500

    build

---
+++ build/precache-manifest.4732d807b48ea310ccf3fae3d770aa6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b55f4a4e774e8b75782b3880e8b001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adc88549de2bff9dc62",
+    "url": "/static/css/main.0807307c.chunk.css"
+  },
+  {
+    "revision": "2611f878c00d63b3dabd",
+    "url": "/static/js/2.052df145.chunk.js"
+  },
+  {
+    "revision": "3d44626c5d461adbaec4b53e86eef37f",
+    "url": "/static/js/2.052df145.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8adc88549de2bff9dc62",
+    "url": "/static/js/main.61617e0a.chunk.js"
+  },
+  {
+    "revision": "2ddbfc8f025ebc487639",
+    "url": "/static/js/runtime-main.79b955ce.js"
+  },
+  {
+    "revision": "c9a309ec297e61e011df3c2ce90f8b51",
+    "url": "/static/media/logo.c9a309ec.jpg"
+  }
+]);
