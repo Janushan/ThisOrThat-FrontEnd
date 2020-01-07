36a6c52692049b1b9af6d153e2d481651b0630d7
Janu Shan <janu47@live.com>
2020-01-07 13:58:22 +0000

    rebuilt

---
+++ build/precache-manifest.5e5ac1942eb6b484fa72f92ef69b25bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec781c15dc25b3b27b6ebba140e2a52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "138f899c64d482bfa04c",
+    "url": "/static/css/main.dce56b9b.chunk.css"
+  },
+  {
+    "revision": "dd622fe11ca0767f8eb1",
+    "url": "/static/js/2.8708c8ab.chunk.js"
+  },
+  {
+    "revision": "3d44626c5d461adbaec4b53e86eef37f",
+    "url": "/static/js/2.8708c8ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "138f899c64d482bfa04c",
+    "url": "/static/js/main.bc928854.chunk.js"
+  },
+  {
+    "revision": "2ddbfc8f025ebc487639",
+    "url": "/static/js/runtime-main.79b955ce.js"
+  },
+  {
+    "revision": "c9a309ec297e61e011df3c2ce90f8b51",
+    "url": "/static/media/logo.c9a309ec.jpg"
+  }
+]);
